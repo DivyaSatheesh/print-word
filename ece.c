73aa6f8fa56995d7047293a5187a226a3d87af61
DivyaSatheesh <sk.divya69@gmail.com>
2016-07-18 19:10:26 +0530

    Create ece.c

---
+++ ece.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[50];
+int n,i;
+clrscr();
+scanf("%s",a);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%s\n",a);
+}
+getch();
+return 0;
+}
